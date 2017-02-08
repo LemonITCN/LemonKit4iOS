aea45b2a6ff8e8caf745d615b2ffae72f915792f
LiuRi <liuri@lemonsoft.net>
2017-02-08 17:53:01 +0800

    临时保存，准备放置columns位置

--- LemonKit/LKSamples/ViewController/Main/DemoViewController.m
+++ LemonKit/LKSamples/ViewController/Main/DemoViewController.m
@@ -24,11 +24,11 @@
     // 计算控件的位置
     self->_rootLinkageView = [[LKLinkageView alloc] initWithFrame: CGRectMake(0, NAVIGATION_BAR_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - NAVIGATION_BAR_HEIGHT - TAB_BAR_HEIGHT)];
     [self addSubview: self->_rootLinkageView];
+    // 读取本地demo目录plist
     NSDictionary *linkageInfo = [NSDictionary dictionaryWithMainBundlePlistName: @"LKDemoLinkageInfo"];
-    LKLog(@"rootitems count : %lu", (unsigned long)((NSArray *)linkageInfo[@"rootItems"]).count);
-    for (NSDictionary *dic in linkageInfo[@"rootItems"]) {
+    // 遍历，根据dictionary生成对象
+    for (NSDictionary *dic in linkageInfo[@"rootItems"]) 
         [self->_rootLinkageView.rootItems addObject: [[LKLinkageItem alloc] initWithStandardDictionary: dic]];
-    }
     [self->_rootLinkageView refresh];// 刷新，应用内容
 }
 
--- LemonKit/LemonKit/LKViewExt/LKLinkageView.m
+++ LemonKit/LemonKit/LKViewExt/LKLinkageView.m
@@ -8,25 +8,53 @@
 
 #import "LKLinkageView.h"
 #import "LKLinkageItem.h"
+#import "LKLinkageColumn.h"
+
+@interface LKLinkageView(){
+    NSMutableArray<LKLinkageColumn *> *_columns;
+}
+
+@end
 
 @implementation LKLinkageView
 
 - (instancetype)init{
     if (self = [super init]) {
-        self->_rootItems = [NSMutableArray new];
+        [self _init];
     }
     return self;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame{
     if (self = [super initWithFrame: frame]) {
-        self->_rootItems = [NSMutableArray new];
+        [self _init];
     }
     return self;
 }
 
+- (void)_init{
+    self->_rootItems = [NSMutableArray new];
+    self->_columns = [NSMutableArray new];
+}
+
 - (void)refresh{
-    
+    // 根据深度处理columns
+    [self autoDealColumns];
+}
+
+- (void)autoDealColumns{
+    NSInteger maxDeepth = 0;
+    // 遍历计算深度
+    for (LKLinkageItem *item in self->_rootItems)
+        maxDeepth = MAX(maxDeepth, [item calculateDepth]);
+    if (self->_columns.count > maxDeepth)
+        // 有多余的column
+        [self->_columns removeObjectsInRange: NSMakeRange(maxDeepth, self->_columns.count - maxDeepth)];
+    else if (self->_columns.count < maxDeepth)
+        // column不够用，补足缺少的
+        for (NSInteger i = 0 ; i < maxDeepth - self->_columns.count ; i ++)
+            [self->_columns addObject: [[LKLinkageColumn alloc] init]];
+    // 接下来，根据当前的状态重新摆放column
 }
 
 @end
